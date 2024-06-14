0b07aec16d2fe17f529697d318e93140071469c2
Indu Prabha <apindhuece12@gmail.com>
2024-06-13 22:48:50 -0400

    Movie display and form is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+import {useState, useEffect} from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import './App.css'
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
-function App() {
-  
+export default function App() {
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    const data = await response.json();
+    setMovie(data);
+  };
+
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
 
   return (
-    <>
-     
- 
-    </>
-  )
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
 }
-
-export default App
